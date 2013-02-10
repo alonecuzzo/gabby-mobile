e21191330fe023f89bbd6f79c5b71249603fafa0
Zainab 3brahimi <zainabebrahimi@gmail.com>
2013-02-10 07:21:39 -0500

    working on hearstService plus view

--- Resources/services/HearstService.js
+++ Resources/services/HearstService.js
@@ -5,31 +5,34 @@ function HearstService(){
 	
 	self.lolz = 'tehlolz';
 	
-	self.myFunction = function() {
+	//self.articles = function() {
+		var url ='http://hearst.api.mashery.com/Article/search?_pretty=0&shape=brief&start=0&limit=10&sort=publish_date%2Cdesc&total=0&api_key=4mpxekczdccmv22wrf9uqq9j';
+		var response, json;
 		
-	};
+		var client = Ti.Network.createHTTPClient({
+	     // function called when the response data is available
+	     onload : function(e) {
+	         //Ti.API.info("Received text: " + this.responseText);
+         	 //alert('success');
+	         Ti.App.fireEvent('eventHearst', {json:this.responseText});
+	     },
+	     // function called when an error occurs, including a timeout
+	     onerror : function(e) {
+	         Ti.API.debug(e.error);
+	         alert('error');
+	     },
+	     timeout : 5000  // in milliseconds
+	 });
+	 
+		 client.open("GET", url);
+		 client.send();
+		 
+		
+		
+	//};
 	
 	// var articleId = 15075870;
-	// var url ='http://hearst.api.mashery.com/Article/search?_pretty=1&shape=brief&id='+articleId+'&start=0&total=0&api_key=4mpxekczdccmv22wrf9uqq9j';
-// 	
-	// var client = Ti.Network.createHTTPClient({
-	     // // function called when the response data is available
-	     // onload : function(e) {
-	         // // Ti.API.info("Received text: " + this.responseText);
-	         // //alert('here!');
-	         // var response = JSON.parse(this.responseText);
-	         // alert(response);
-	     // },
-	     // // function called when an error occurs, including a timeout
-	     // onerror : function(e) {
-	         // Ti.API.debug(e.error);
-	         // alert('error');
-	     // },
-	     // timeout : 5000  // in milliseconds
-	 // });
-// 	 
-		 // client.open("GET", url);
-		 // client.send();
+	
 		
 	return self;	
 		 
